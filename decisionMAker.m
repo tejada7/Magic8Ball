6321f39c7e903ab0107f7ab14e529ab96814cc31
tejada7 <Favio@Favios-iMac.local>
2016-05-11 20:32:45 +0200

    Added 20 messages

--- decisionMAker.m
+++ decisionMAker.m
@@ -21,6 +21,21 @@
         [_array addObject:@"Why not?"];
         [_array addObject:@"Pour quoi pas?"];
         [_array addObject:@"Are you kidding me?"];
+        [_array addObject:@"Seriously?"];
+        [_array addObject:@"What about beers?"];
+        [_array addObject:@"Do you wanna dance?"];
+        [_array addObject:@"Perfect?"];
+        [_array addObject:@"I don-t think so"];
+        [_array addObject:@"I like IOS"];
+        [_array addObject:@"Most likely"];
+        [_array addObject:@"My sources say no?"];
+        [_array addObject:@"No way"];
+        [_array addObject:@"Tu es fada?"];
+        [_array addObject:@"You are crazy"];
+        [_array addObject:@"Absolutely yes"];
+        [_array addObject:@"why?"];
+        [_array addObject:@"Are you drunk?"];
+        [_array addObject:@"Good idea!!"];
     }
 }
 
@@ -29,7 +44,7 @@
 {
     [self initializeArray];
     //generating the random number
-    int randomNumber = arc4random() %(4	) - 0;
+    int randomNumber = arc4random() %(20) - 0;
     return [_array objectAtIndex:randomNumber];
 }
 
