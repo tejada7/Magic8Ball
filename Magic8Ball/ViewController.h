e5e512aa1f202129594c70aaae725e90447b7c59
tejada7 <Favio@favios-imac.local>
2016-05-11 18:51:13 +0200

    Initial Commit

---
+++ Magic8Ball/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Magic8Ball
+//
+//  Created by Favio on 5/11/16.
+//  Copyright © 2016 Favio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Magic8BallTests/Magic8BallTests.m
@@ -0,0 +1,39 @@
+//
+//  Magic8BallTests.m
+//  Magic8BallTests
+//
+//  Created by Favio on 5/11/16.
+//  Copyright © 2016 Favio. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Magic8BallTests : XCTestCase
+
+@end
+
+@implementation Magic8BallTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
